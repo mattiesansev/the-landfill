ae574fd849df74372089db903f6d3d06a6dff6c6
Nick Vargas <nicolas.vargas2016@gmail.com>
2024-10-08 23:59:34 -0700

    map layers and some limited new styling exploration

--- client/src/pages/posts/LandfillMap.jsx
+++ client/src/pages/posts/LandfillMap.jsx
@@ -5,6 +5,7 @@ import {
   MapContainer,
   TileLayer,
   LayersControl,
+  LayerGroup,
   Polygon,
   Popup,
 } from "react-leaflet"
@@ -73,83 +74,127 @@ const LandfillMap = () => {
             zoom={12}
             style={{ height: "100vh", width: "100%" }}
           >
-            <LayersControl position="topright">
-              <LayersControl.BaseLayer checked name="OpenStreetMap">
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              </LayersControl.BaseLayer>
-              <LayersControl.BaseLayer name="Satellite">
-                <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
-              </LayersControl.BaseLayer>
+            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <LayersControl position="topright" collapsed={false}>
+              <LayersControl.Overlay name="Layer 1 Polygons">
+                <LayerGroup>
+                  {coordinatesPerLandfill &&
+                    coordinatesPerLandfill.map((polyCoordinatePerLandfill) => {
+                      //console.log("coords per landfill, ", coordinatesPerLandfill)
+                      let lats = []
+                      let lons = []
+                      try {
+                        lats = JSON.parse(polyCoordinatePerLandfill.Latitude)
+                        lons = JSON.parse(polyCoordinatePerLandfill.Longitude)
+                      } catch (e) {
+                        console.log("invalid row ", polyCoordinatePerLandfill)
+                      }
+                      //console.log("lats and longs ", lats, lons)
+                      let polygonCoords = []
+                      let text = polyCoordinatePerLandfill.LandfillName
+                      for (let i = 0; i < lats.length; i++) {
+                        const lat = lats[i]
+                        const lon = 0 - lons[i]
+                        polygonCoords.push([lat, lon])
+                        // const id = coordinatePerLandfill.SWIS
+                        console.log("polygonCoords", polygonCoords)
+                      }
+                      return (
+                        <>
+                          <Polygon
+                            pathOptions={{ color: "blue" }}
+                            positions={polygonCoords}
+                          >
+                            <Popup className="popup">
+                              <p className="popUpTitle">{text}</p>
+                              <p className="popUpText">
+                                This landfill has different types of waste!
+                              </p>
+                            </Popup>
+                          </Polygon>
+                        </>
+                      )
+                    })}
+                </LayerGroup>
+              </LayersControl.Overlay>
+
+              <LayersControl.Overlay name="Layer 2 Polygons">
+                <LayerGroup>
+                  {coordinatesPerLandfill &&
+                    coordinatesPerLandfill.map((polyCoordinatePerLandfill) => {
+                      //console.log("coords per landfill, ", coordinatesPerLandfill)
+                      let lats = []
+                      let lons = []
+                      try {
+                        lats = JSON.parse(polyCoordinatePerLandfill.Latitude)
+                        lons = JSON.parse(polyCoordinatePerLandfill.Longitude)
+                      } catch (e) {
+                        console.log("invalid row ", polyCoordinatePerLandfill)
+                      }
+                      //console.log("lats and longs ", lats, lons)
+                      let polygonCoords = []
+                      let text = polyCoordinatePerLandfill.LandfillName
+                      for (let i = 0; i < lats.length; i++) {
+                        const lat = lats[i]
+                        const lon = 0 - lons[i]
+                        polygonCoords.push([lat, lon])
+                        // const id = coordinatePerLandfill.SWIS
+                        console.log("polygonCoords", polygonCoords)
+                      }
+                      return (
+                        <>
+                          <Polygon
+                            pathOptions={{ color: "red" }}
+                            positions={polygonCoords}
+                          >
+                            <Popup className="popup">
+                              <p className="popUpTitle">{text}</p>
+                              <p className="popUpText">
+                                This landfill is located at {lats[0]}, {lons[0]}
+                              </p>
+                              <p className="popUpText">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Aliquam posuere ante
+                                vestibulum, tristique leo malesuada, maximus
+                                tellus. Curabitur iaculis sit amet nulla a
+                                condimentum. Phasellus sit amet eros aliquet,
+                                consequat lacus id, tristique tortor. Praesent
+                                euismod enim a gravida convallis. Mauris non
+                                elit nec felis scelerisque fermentum. Integer
+                                porttitor pulvinar dui non posuere. Praesent eu
+                                risus sed massa tincidunt lacinia ac non mauris.
+                                Cras at pellentesque massa. Donec facilisis,
+                                enim viverra gravida ultrices, leo est viverra
+                                augue, in ultrices leo dolor sit amet libero. In
+                                et libero ut quam scelerisque porta et et ipsum.
+                                Suspendisse rhoncus tellus non leo imperdiet
+                                posuere. Proin facilisis nisl sit amet ultrices
+                                venenatis. Ut sem mauris, fermentum eget
+                                vestibulum vel, mollis eget quam. In id elit
+                                dolor. Integer at eros vel sem imperdiet
+                                finibus. Cras nec vestibulum lacus.
+                                <br />
+                                <br />
+                                Vestibulum sit amet iaculis lorem, in sagittis
+                                mauris. Vestibulum vitae massa magna. Proin
+                                vitae tellus porttitor, finibus sem suscipit,
+                                luctus neque. Vivamus vitae libero quis lectus
+                                malesuada hendrerit in in diam. Mauris egestas
+                                rutrum mauris, vel luctus tortor rutrum vitae.
+                                Pellentesque vestibulum turpis non condimentum
+                                malesuada. Quisque et velit nisl. Cras non
+                                rutrum enim, at facilisis sem. Mauris sed rutrum
+                                turpis. Pellentesque nec est sollicitudin,
+                                iaculis magna id, tristique ante.
+                              </p>
+                            </Popup>
+                          </Polygon>
+                        </>
+                      )
+                    })}
+                </LayerGroup>
+              </LayersControl.Overlay>
             </LayersControl>
-            {coordinatesPerLandfill &&
-              coordinatesPerLandfill.map((polyCoordinatePerLandfill) => {
-                //console.log("coords per landfill, ", coordinatesPerLandfill)
-                let lats = []
-                let lons = []
-                try {
-                  lats = JSON.parse(polyCoordinatePerLandfill.Latitude)
-                  lons = JSON.parse(polyCoordinatePerLandfill.Longitude)
-                } catch (e) {
-                  console.log("invalid row ", polyCoordinatePerLandfill)
-                }
-                //console.log("lats and longs ", lats, lons)
-                let polygonCoords = []
-                let text = polyCoordinatePerLandfill.LandfillName
-                for (let i = 0; i < lats.length; i++) {
-                  const lat = lats[i]
-                  const lon = 0 - lons[i]
-                  polygonCoords.push([lat, lon])
-                  // const id = coordinatePerLandfill.SWIS
-                  console.log("polygonCoords", polygonCoords)
-                }
-                return (
-                  <>
-                    <Polygon
-                      pathOptions={{ color: "red" }}
-                      positions={polygonCoords}
-                    >
-                      <Popup className="popup">
-                        <p className="popUpTitle">{text}</p>
-                        <p className="popUpText">
-                          This landfill is located at {lats[0]}, {lons[0]}
-                        </p>
-                        <p className="popUpText">
-                          Lorem ipsum dolor sit amet, consectetur adipiscing
-                          elit. Aliquam posuere ante vestibulum, tristique leo
-                          malesuada, maximus tellus. Curabitur iaculis sit amet
-                          nulla a condimentum. Phasellus sit amet eros aliquet,
-                          consequat lacus id, tristique tortor. Praesent euismod
-                          enim a gravida convallis. Mauris non elit nec felis
-                          scelerisque fermentum. Integer porttitor pulvinar dui
-                          non posuere. Praesent eu risus sed massa tincidunt
-                          lacinia ac non mauris. Cras at pellentesque massa.
-                          Donec facilisis, enim viverra gravida ultrices, leo
-                          est viverra augue, in ultrices leo dolor sit amet
-                          libero. In et libero ut quam scelerisque porta et et
-                          ipsum. Suspendisse rhoncus tellus non leo imperdiet
-                          posuere. Proin facilisis nisl sit amet ultrices
-                          venenatis. Ut sem mauris, fermentum eget vestibulum
-                          vel, mollis eget quam. In id elit dolor. Integer at
-                          eros vel sem imperdiet finibus. Cras nec vestibulum
-                          lacus.
-                          <br />
-                          <br />
-                          Vestibulum sit amet iaculis lorem, in sagittis mauris.
-                          Vestibulum vitae massa magna. Proin vitae tellus
-                          porttitor, finibus sem suscipit, luctus neque. Vivamus
-                          vitae libero quis lectus malesuada hendrerit in in
-                          diam. Mauris egestas rutrum mauris, vel luctus tortor
-                          rutrum vitae. Pellentesque vestibulum turpis non
-                          condimentum malesuada. Quisque et velit nisl. Cras non
-                          rutrum enim, at facilisis sem. Mauris sed rutrum
-                          turpis. Pellentesque nec est sollicitudin, iaculis
-                          magna id, tristique ante.
-                        </p>
-                      </Popup>
-                    </Polygon>
-                  </>
-                )
-              })}
           </MapContainer>
         </div>
       </div>
