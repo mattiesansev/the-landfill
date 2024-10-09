51bc6883ec877e37b59712599de3ad45150e66b7
Nick Vargas <nicolas.vargas2016@gmail.com>
2024-10-08 22:29:57 -0700

    prettier and one map layer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import AuthorFooter from "../components/AuthorFooter";
-import { authors } from '../authors/authors'
+import { authors } from "../authors/authors";
 
 const Home = () => {
   const posts = [
     {
       id: "bay-area-landfills",
-      title: "An analysis and visualization of abandoned and illegal landfills across the Bay Area",
+      title:
+        "An analysis and visualization of abandoned and illegal landfills across the Bay Area",
       desc: "We researched and visualized all of the abandoned landfills across the bay area.",
       img: "https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU",
     },
@@ -48,10 +49,22 @@ const Home = () => {
               <div className="content">
                 <div className="title">{post.title}</div>
                 <div className="authors">
-                <AuthorFooter authorImageUrl={authors["mattie"]["photo"]} postDate='July 15, 2024' authorName={authors["mattie"]["name"]} />
-                <AuthorFooter authorImageUrl={authors["destiny"]["photo"]} postDate='July 15, 2024' authorName={authors["destiny"]["name"]} />            
-                <AuthorFooter authorImageUrl={authors["destiny"]["photo"]} postDate='July 15, 2024' authorName={authors["destiny"]["name"]} />            
-              </div>
+                  <AuthorFooter
+                    authorImageUrl={authors["mattie"]["photo"]}
+                    postDate="July 15, 2024"
+                    authorName={authors["mattie"]["name"]}
+                  />
+                  <AuthorFooter
+                    authorImageUrl={authors["destiny"]["photo"]}
+                    postDate="July 15, 2024"
+                    authorName={authors["destiny"]["name"]}
+                  />
+                  <AuthorFooter
+                    authorImageUrl={authors["destiny"]["photo"]}
+                    postDate="July 15, 2024"
+                    authorName={authors["destiny"]["name"]}
+                  />
+                </div>
               </div>
             </Link>
           </div>
