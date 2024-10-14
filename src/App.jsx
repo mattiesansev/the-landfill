4369f571126d9fb9e082e7b7df45f7951cfe3481
Nick Vargas <nicolas.vargas2016@gmail.com>
2024-10-13 21:28:40 -0700

    hashrouter refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,4 @@
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Outlet,
-  HashRouter as Router,
-} from "react-router-dom"
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 import Single from "./pages/Single"
 import Home from "./pages/Home"
 import Navbar from "./components/Navbar"
@@ -14,44 +8,34 @@ import LandfillMap from "./pages/posts/LandfillMap"
 import AboutUs from "./pages/AboutUs"
 import TitleRow from "./components/TitleRow"
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <>
       <TitleRow />
-      <Outlet />
+      {children}
       <Footer />
     </>
   )
 }
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/post/bay-area-landfills",
-        element: <LandfillMap />,
-      },
-      {
-        path: "/post/about",
-        element: <AboutUs />,
-      },
-      // {
-      //   path:"/post/contact",
-      //   element: <Contact/>
-      // },
-    ],
-  },
-])
+
 function App() {
   return (
     <div className="app">
       <div className="container">
-        <RouterProvider router={router} />
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route
+                path="/post/bay-area-landfills"
+                element={<LandfillMap />}
+              />
+              <Route path="/post/about" element={<AboutUs />} />
+              {/* Uncomment when Contact component is ready */}
+              {/* <Route path="/post/contact" element={<Contact />} /> */}
+            </Routes>
+          </Layout>
+        </Router>
       </div>
     </div>
   )
