181af29b9b31327b052d3731c6be7ce804a9f4ff
Nick Vargas <nicolas.vargas2016@gmail.com>
2024-10-13 21:44:43 -0700

    public csv folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,15 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/the-landfill/",
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        csv: "public/grouped_xy_with_info.csv",
+      },
+    },
+  },
 })
