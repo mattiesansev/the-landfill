c4881f65b0f737f55ab9bf7987ae4744a643f52d
Nick Vargas <nicolas.vargas2016@gmail.com>
2024-10-27 17:28:42 -0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +8,7 @@ export default defineConfig({
     outDir: "dist",
     assetsDir: "assets",
     copyPublicDir: true,
-  }
-});
+    modulePreload: true,
+    target: "esnext",
+  },
+})
