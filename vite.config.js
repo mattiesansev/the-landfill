2fb03e192f95d850a5b168bad79b47cbe8ad3059
Nick Vargas <nicolas.vargas2016@gmail.com>
2024-10-27 17:38:02 -0700

    dist base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   build: {
-    base: "/",
+    base: "/dist/",
     outDir: "dist",
     assetsDir: "assets",
     copyPublicDir: true,
