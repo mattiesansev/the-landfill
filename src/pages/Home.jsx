c4806345399c93e5b28dedf7b74e74565049306a
mattiesansev <mesansev@gmail.com>
2024-12-08 11:52:24 -0800

    Trying pixels

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,8 +4,20 @@ import AuthorFooter from "../components/AuthorFooter";
 import { authors } from "../authors/authors";
 import landfillCover from "/src/header.jpg"
 import aboutUsCover from "../img/group_photo.jpg"
+import ReactGA from 'react-ga4';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom'; 
+
+
+ReactGA.initialize('G-NR2T70PVBG'); 
 
 const Home = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: location.pathname });
+  }, [location]);
+
   const posts = [
     {
       id: "bay-area-landfills",
